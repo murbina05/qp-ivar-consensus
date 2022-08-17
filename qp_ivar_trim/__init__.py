88e16f3f344d236ce2828ff886140fd29539235f
murbina05 <109252973+murbina05@users.noreply.github.com>
2022-08-17 12:36:42 -0700

    Fixing index out of range

--- qp_ivar_trim/__init__.py
+++ qp_ivar_trim/__init__.py
@@ -22,9 +22,8 @@ plugin = QiitaPlugin(**plugin_details)
 
 # Define the command
 dbs = get_dbs_list()
-sys.stdout.write(dbs)
-dbs_without_extension = [splitext(db)[0] for db in dbs]
 sys.stdout.write(str(dbs))
+dbs_without_extension = [splitext(db)[0] for db in dbs]
 dbs_defaults = ', '.join([f'"{x}"' for x in dbs_without_extension])
 req_params = {'input': ('artifact', ['per_sample_FASTQ'])}
 opt_params = {
